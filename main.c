0e406a52fa41f1652f280edfd640c882f2a69f3c
codeByVaibhav <k.vaibhav9013@gmail.com>
2022-07-06 05:07:12 +0530

    updated game code

--- main.c
+++ main.c
@@ -10,40 +10,37 @@
 
 */
 
-#include <windows.h>
+#include <conio.h>
+#include <math.h>
 #include <stdio.h>
 #include <time.h>
-#include <math.h>
-#include <conio.h>
+#include <windows.h>
 
 #define SCREEN_WIDTH 120
 #define SCREEN_HEIGHT 40
 
-// #define SCREEN_WIDTH 480
-// #define SCREEN_HEIGHT 360
-
 const int SCREEN_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT;
+const int SCREEN_WIDTH_HALF = SCREEN_WIDTH / 2;
+const int SCREEN_HEIGHT_HALF = SCREEN_HEIGHT / 2;
 
 int eaten = 0;
 
-enum DIRECTION
-{
+enum DIRECTION {
     UP,
     DOWN,
     LEFT,
     RIGHT
 };
 
-struct Vec2D
-{
+struct Vec2D {
     float x, y;
 } snake, speed, foodpos, tail[1000];
 
 struct Vec2D directions[4] = {
-    {0, 1},  // UP
-    {0, -1}, // DOWN
-    {-1, 0}, // LEFT
-    {1, 0}   // RIGHT
+    {0, 1},   // UP
+    {0, -1},  // DOWN
+    {-1, 0},  // LEFT
+    {1, 0}    // RIGHT
 };
 
 unsigned short KeyEventProc(KEY_EVENT_RECORD ker);
@@ -56,8 +53,7 @@ void clearscr(char *scr);
 void getfood(char *scr, struct Vec2D *pos);
 void pause(unsigned int wtime);
 
-int main()
-{
+int main() {
     srand(time(NULL));
     system("color 3");
     system("mode con: cols=120 lines=40");
@@ -77,7 +73,7 @@ int main()
     hStdin = GetStdHandle(STD_INPUT_HANDLE);
     fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
     SetConsoleMode(hStdin, fdwMode);
-    SetConsoleTitle("Snake Game BY: Vaibhav");
+    SetConsoleTitle("Snake Game BY: KV");
     unsigned short keyPressed;
     //=================================================================================================================================
     char q = 254;
@@ -90,39 +86,28 @@ int main()
     speed = directions[RIGHT];
 
     //===========================================================================================================
-    while (1)
-    {
+    while (1) {
         //=======================================READING CONSOLE INPUT=================================================
-        if (kbhit())
-        {
+        if (kbhit()) {
             ReadConsoleInput(
-                hStdin,     // input buffer handle
-                irInBuf,    // buffer to read into
-                128,        // size of read buffer
-                &cNumRead); // number of records read
+                hStdin,      // input buffer handle
+                irInBuf,     // buffer to read into
+                128,         // size of read buffer
+                &cNumRead);  // number of records read
 
-            for (i = 0; i < cNumRead; i++)
-            {
-                if (irInBuf[i].EventType == KEY_EVENT)
-                {
+            for (i = 0; i < cNumRead; i++) {
+                if (irInBuf[i].EventType == KEY_EVENT) {
                     keyPressed = KeyEventProc(irInBuf[i].Event.KeyEvent);
-                    if (keyPressed == VK_UP && speed.x != 0)
-                    {
+                    if (keyPressed == VK_UP && speed.x != 0) {
                         speed = directions[UP];
                         break;
-                    }
-                    else if (keyPressed == VK_DOWN && speed.x != 0)
-                    {
+                    } else if (keyPressed == VK_DOWN && speed.x != 0) {
                         speed = directions[DOWN];
                         break;
-                    }
-                    else if (keyPressed == VK_LEFT && speed.y != 0)
-                    {
+                    } else if (keyPressed == VK_LEFT && speed.y != 0) {
                         speed = directions[LEFT];
                         break;
-                    }
-                    else if (keyPressed == VK_RIGHT && speed.y != 0)
-                    {
+                    } else if (keyPressed == VK_RIGHT && speed.y != 0) {
                         speed = directions[RIGHT];
                         break;
                     }
@@ -131,26 +116,21 @@ int main()
         }
         //===========================================================================================================
         // getting food for snake.
-        if (eaten == 0)
-        {
-            getfood(screen, &foodpos);
-        }
+        if (eaten == 0) getfood(screen, &foodpos);
+
         //====================================COLLISION DETECTION=====================================================
 
         // snake and food collision detection.
-        if (snake.x == foodpos.x && snake.y == foodpos.y)
-        {
+        if (snake.x == foodpos.x && snake.y == foodpos.y) {
             eaten = 0;
             len++;
         }
 
         // snake and tail collision detection.
-        for (int i = 0; i < len; i++)
-        {
-            if (snake.x == tail[i].x && snake.y == tail[i].y)
-            {
+        for (int i = 0; i < len; i++) {
+            if (snake.x == tail[i].x && snake.y == tail[i].y) {
                 printf("\a");
-                pause(2000);
+                pause(1800);
                 printf("\a");
                 len = 0;
             }
@@ -163,12 +143,10 @@ int main()
 
         //======================================SNAKE MOVEMENT UPDATE======================================================
 
-        rate = 250 - 2 * (len / 2); // as the snake length increses the speed  of the snake will also increase.
+        rate = 250 - 2 * (len / 2);  // as the snake length increses the speed  of the snake will also increase.
 
-        if (counter % rate == 0)
-        {
-            for (int i = len; i > 0; i--)
-            {
+        if (counter % rate == 0) {
+            for (int i = len; i > 0; i--) {
                 tail[i] = tail[i - 1];
             }
             tail[0] = snake;
@@ -188,8 +166,7 @@ int main()
         draw(snake.x, snake.y, screen, q, 0, 0);
 
         // snake tail
-        for (int i = 0; i < len; i++)
-        {
+        for (int i = 0; i < len; i++) {
             draw(tail[i].x, tail[i].y, screen, q, 0, 0);
         }
 
@@ -221,78 +198,63 @@ int main()
 //========================DEFINING ALL FUNCTIONS===================================================
 
 // FUNCTION FOR KEYBOARD EVENT HANDELING.
-unsigned short KeyEventProc(KEY_EVENT_RECORD ker)
-{
-    if (ker.bKeyDown)
-    {
+unsigned short KeyEventProc(KEY_EVENT_RECORD ker) {
+    if (ker.bKeyDown) {
         return ker.wVirtualKeyCode;
     }
 }
 // FUNCTION FOR WRITING STRING ON SCREEN.
-void write(float x, float y, char *str, char *scr)
-{
-    for (int i = 0; str[i] != '\0'; i++)
-    {
+void write(float x, float y, char *str, char *scr) {
+    for (int i = 0; str[i] != '\0'; i++) {
         draw(x + i, y, scr, str[i], 0, 0);
     }
 }
 // FUNCTION FOR GIVING RANDOM POSITION TO FOOD.
-void getfood(char *scr, struct Vec2D *pos)
-{
+void getfood(char *scr, struct Vec2D *pos) {
     pos->x = -((rand() % 29)) + ((rand() % 29));
     pos->y = -((rand() % 19)) + ((rand() % 19));
     eaten = 1;
 }
 
 // FUNCTION FOR PAUSING IN GAME.
-void pause(unsigned int wtime)
-{
+void pause(unsigned int wtime) {
     clock_t ptime = wtime + clock();
     while (ptime >= clock())
         ;
 }
 // FUNCTION FOR DRAWING A CHAR ON SCREEN. GIVEN ITS X ANY Y POSITION AND CHAR P.
-void draw(float x, float y, char *scr, char p, float xoff, float yoff)
-{
-    x = (int)(2 * (x + xoff));
-    y = (int)(-(y + yoff));
+void draw(float x, float y, char *scr, char p, float xoff, float yoff) {
+    x = (int)(((x + xoff)) + SCREEN_WIDTH_HALF);
+    y = (int)((-(y + yoff)) + SCREEN_HEIGHT_HALF);
 
-    int drawon = ((y + SCREEN_HEIGHT / 2) * SCREEN_WIDTH) + (x + SCREEN_WIDTH / 2);
+    int drawon = SCREEN_WIDTH * y + x;
 
-    if (drawon < SCREEN_SIZE && drawon >= 0)
-    {
+    if (drawon < SCREEN_SIZE && drawon >= 0) {
         scr[drawon] = p;
     }
 }
 // FUNCTION FOR CLEARING THE SCREEN.
-void clearscr(char *scr)
-{
-    int j;
-    for (j = 0; j < SCREEN_SIZE; j++)
-    {
-        scr[j] = ' ';
+void clearscr(char *scr) {
+    for (int i = 0; i < SCREEN_SIZE; i++) {
+        scr[i] = ' ';
     }
     scr[SCREEN_SIZE - 1] = '\0';
 }
 
-void plotLineLow(float x0, float y0, float x1, float y1, char *scr, char p, float xoff, float yoff)
-{
+void plotLineLow(float x0, float y0, float x1, float y1, char *scr, char p, float xoff, float yoff) {
     float dx = x1 - x0;
     float dy = y1 - y0;
     float yi = 1;
-    if (dy < 0)
-    {
+    if (dy < 0) {
         yi = -1;
         dy = -dy;
     }
     float D = 2 * dy - dx;
     float y = y0;
 
-    for (int x = x0; x <= x1; x++)
-    {
+    for (int x = x0; x <= x1; x++) {
         draw(x, y, scr, p, xoff, yoff);
-        if (D > 0)
-        {
+        if (D > 0) {
             y = y + yi;
             D = D - 2 * dx;
         }
@@ -300,24 +262,20 @@ void plotLineLow(float x0, float y0, float x1, float y1, char *scr, char p, floa
     }
 }
 
-void plotLineHigh(float x0, float y0, float x1, float y1, char *scr, char p, float xoff, float yoff)
-{
+void plotLineHigh(float x0, float y0, float x1, float y1, char *scr, char p, float xoff, float yoff) {
     float dx = x1 - x0;
     float dy = y1 - y0;
     float xi = 1;
-    if (dx < 0)
-    {
+    if (dx < 0) {
         xi = -1;
         dx = -dx;
     }
     float D = 2 * dx - dy;
     float x = x0;
 
-    for (int y = y0; y <= y1; y++)
-    {
+    for (int y = y0; y <= y1; y++) {
         draw(x, y, scr, p, xoff, yoff);
-        if (D > 0)
-        {
+        if (D > 0) {
             x = x + xi;
             D = D - 2 * dy;
         }
@@ -325,25 +283,16 @@ void plotLineHigh(float x0, float y0, float x1, float y1, char *scr, char p, flo
     }
 }
 
-void drawline(float x0, float y0, float x1, float y1, char *scr, char p, float xoff, float yoff)
-{
-    if (abs(y1 - y0) < abs(x1 - x0))
-    {
-        if (x0 > x1)
-        {
+void drawline(float x0, float y0, float x1, float y1, char *scr, char p, float xoff, float yoff) {
+    if (abs(y1 - y0) < abs(x1 - x0)) {
+        if (x0 > x1) {
             plotLineLow(x1, y1, x0, y0, scr, p, xoff, yoff);
-        }
-        else
-        {
+        } else {
             plotLineLow(x0, y0, x1, y1, scr, p, xoff, yoff);
         }
-    }
-    else if (y0 > y1)
-    {
+    } else if (y0 > y1) {
         plotLineHigh(x1, y1, x0, y0, scr, p, xoff, yoff);
-    }
-    else
-    {
+    } else {
         plotLineHigh(x0, y0, x1, y1, scr, p, xoff, yoff);
     }
 }
